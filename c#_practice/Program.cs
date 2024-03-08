1c76556cb052443e4f076ea805488c8ebae1547d
Jason <devop.jas@gmail.com>
2024-03-08 18:46:58 +0800

    sdsdsd

--- c#_practice/Program.cs
+++ c#_practice/Program.cs
@@ -3,8 +3,8 @@
 
 
 //fixed array
-int[] intArr = new int[10]; //reference type & it will be store in the heap
-string[] stringArr = new string[10];
+//int[] intArr = new int[10]; //reference type & it will be store in the heap
+/*string[] stringArr = new string[10];
 object[] objectArr = new object[10];
 
 intArr[0] = 1;//manipulating the spot | zero based index
@@ -22,4 +22,42 @@ Console.WriteLine(dynamicArray.Average());
 //dynamic string array
 string[] stringDynamicArr = new string[] { "Jason", "Paclibar", "Evaristo" };
 Array.Sort(stringDynamicArr);
-Console.WriteLine("My first name is " + stringDynamicArr[0]);
+Console.WriteLine("My first name is " + stringDynamicArr[0]);*/
+
+
+/*STRINGS IN DEPTH*/
+
+using System.Text;
+
+string testString = "test"; //string is a reference type
+
+//char has a single 
+string testStringStatic = "testStatic";
+
+//IMPORTANT 
+int testInt = 0;
+
+//string in c# are immutable(read only) - Garbage collector
+//Threading - Race Conditions
+string s1 = "hello";
+string s2 = s1;
+s1 += "Word";
+
+
+Console.WriteLine(s2);
+
+// string interpolation + composite formatting 
+string firstName = "Teddy";
+string lastName = "Smith";
+
+Console.WriteLine($"My name is {firstName} {lastName}");
+Console.WriteLine("My name is {1} {0}", lastName, firstName);
+
+
+//String builder
+//if you are performing thousands of iterations: Example: for loop
+
+var sb = new StringBuilder();
+
+var testStringBuilder = sb.AppendLine("dwdd");
+Console.WriteLine(testStringBuilder.ToString());

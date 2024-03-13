195dac1bd41eb9da1f4d13850c47f8f26128a806
Jason <devop.jas@gmail.com>
2024-03-13 14:31:19 +0800

    created an array with list aswell

--- c#_practice/Program.cs
+++ c#_practice/Program.cs
@@ -27,4 +27,19 @@ foreach (int num in fibNumbers)
 for (int i = 0; i < fibNumbers.Count; i++) {
     string forLooop = $"This num {i} came from foor loop";
     Console.WriteLine(forLooop);
+};
+
+
+//we will gonna do it in array
+int[] numbers = new int[] { 3, 2, 4, 5, 3, 5, 6 };
+Console.WriteLine("BELOW ARE THE RESULT OF A FOR-LOOP ARRAY");
+/*foreach(int num in numbers)
+{
+    string number = $"Array value {num}";
+    Console.WriteLine(number);
+}*/
+for (int i = 0; i < numbers.Length; i++)
+{
+    string number = $"Array value: {numbers[i]}";
+    Console.WriteLine(number);
 }
